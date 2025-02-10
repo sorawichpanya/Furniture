8a1a208e865edce359c218205a64356b679a8ec7
sorawichpanya <65010914634@msu.ac.th>
2025-02-10 13:17:16 +0700

    a

--- index1.php
+++ index1.php
@@ -288,7 +288,7 @@
 <div class="container-fluid pt-5">
     <div class="text-center mb-4">
         <h2 class="section-title px-5"><span class="px-2">Trandy Products</span></h2>
-    
+    </div>
         
 <?php
 include_once("connectdb.php");
@@ -326,7 +326,6 @@ while ($data = mysqli_fetch_array($rs)){
             </div>
 <?php } ?>
         </div>
-    </div>
 </div>
 <!-- Products End -->
 
