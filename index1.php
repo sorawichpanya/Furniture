d009ad078aac7177a260b1eb21ec41187b8bbf17
sorawichpanya <65010914634@msu.ac.th>
2025-02-10 13:21:45 +0700

    a

--- index1.php
+++ index1.php
@@ -283,14 +283,6 @@
     <!-- Categories End -->
 
 
-
-<!-- Products Start -->
-<div class="container-fluid pt-5">
-    <div class="text-center mb-4">
-        <h2 class="section-title px-5"><span class="px-2">Trandy Products</span></h2>
-    </div>
-</div>  
-
 <?php
 include_once("connectdb.php");
 
