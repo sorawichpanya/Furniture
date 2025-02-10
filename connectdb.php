d8c1a3f5eaffa261b644536cb1993979ce937143
sorawichpanya <65010914634@msu.ac.th>
2025-02-10 12:15:12 +0700

    a

--- connectdb.php
+++ connectdb.php
@@ -8,13 +8,12 @@
 <body>
 
 <?php
-$servername = "212.80.215.178";
 $host = "localhost";
 $user = "root"; 
 $pass = "12345678P";
 $db = "FurnitureFunny";
 
-$conn = mysqli_connect($servername,$host,$user,$pass) or die ("เชื่อมต่อบ่ได้");
+$conn = mysqli_connect($host,$user,$pass) or die ("เชื่อมต่อบ่ได้");
 
 mysqli_select_db($conn,$db) or die ("เลือกฐานข้อมูลบ่ได้");
 mysqli_query($conn, "set names utf8");
