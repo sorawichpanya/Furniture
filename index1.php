2bcb6b09571f78cf4d833d75960bcebc52c6e890
sorawichpanya <65010914634@msu.ac.th>
2025-02-10 12:22:20 +0700

    a

--- index1.php
+++ index1.php
@@ -300,27 +300,27 @@ $rs = mysqli_query($conn , $sql);
 while ($data = mysqli_fetch_array($rs)){
 
 ?>
-        <div class="row px-xl-5 pb-3">
-            <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
-                <div class="card product-item border-0 mb-4">
-                    <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                        <img class="img-fluid w-100" src="img/product-1.jpg" alt="">
-                    </div>
-                    <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                        <h6 class="text-truncate mb-3">Colorful Stylish Shirt</h6>
-                        <div class="d-flex justify-content-center">
-                            <h6>$123.00</h6>
-                        </div>
-                    </div>
-                    <div class="card-footer d-flex justify-content-between bg-light border">
-                        <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
-                        <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
-                    </div>
-                </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          
+            <img src="../im/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>" >
+            <div class="card-body">
+              <p class="card-text"> 
+			  <?php echo $data['p_name']."<br>"; ?> 
+              <?php echo $data['p_detail']."<br>"; ?>
+              <?php echo $data['p_price']; ?>
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="update.php?id=<?php echo $data['p_id']; ?> "type="button" class="btn btn-sm btn-outline-secondary">แก้ไข</a>
+                  <a href="delete.php?id=<?php echo $data['p_id']; ?>&ext=<?php echo $data['p_ext'];?>" 
+                  type="button" class="btn btn-sm btn-outline-danger" 
+                  onClick="return confirm ('นอนยันนะจ๊ะ');">ลบ</a>
+                </div> 
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-    </div>
     
 <?php } ?>
     <!-- Products End -->
