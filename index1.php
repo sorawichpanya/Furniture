08f0eecc0506bccebfd544a4d1da2a6dfe25f334
sorawichpanya <65010914634@msu.ac.th>
2025-02-10 14:13:54 +0700

    a

--- index1.php
+++ index1.php
@@ -145,7 +145,7 @@
                             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                 <div class="p-3" style="max-width: 700px;">
                                     <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
-                                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">Fashionable Dress</h3>
+                                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">Trendy Products</h3>
                                     <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
                                 </div>
                             </div>
@@ -155,7 +155,7 @@
                             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                 <div class="p-3" style="max-width: 700px;">
                                     <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
-                                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">Reasonable Price</h3>
+                                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">Just Arrived</h3>
                                     <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
                                 </div>
                             </div>
