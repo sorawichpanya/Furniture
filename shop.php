5fc83a33086603d25014b6a71b5b7afa22133667
sorawichpanya <65010914634@msu.ac.th>
2025-03-03 14:07:25 +0700

    a

--- shop.php
+++ shop.php
@@ -240,6 +240,10 @@
                     ?>
                     <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
                         <div class="card product-item border-0 mb-4">
+                                <?php
+                                while ($data = mysqli_fetch_array($rs)){
+
+                                ?>
                             <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
                             <img 
                              src="img/Just_arrived/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>" 
@@ -260,7 +264,7 @@
                             </div>
                         </div>
                         <?php
-                        
+                        }
                         ?>
                     </div>
                     <div class="col-12 pb-1">
