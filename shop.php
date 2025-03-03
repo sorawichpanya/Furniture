62d671d9f6a544c9504162142bd3691c17ca4158
sorawichpanya <65010914634@msu.ac.th>
2025-03-03 14:05:33 +0700

    ฟ

--- shop.php
+++ shop.php
@@ -241,8 +241,12 @@
                     <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
                         <div class="card product-item border-0 mb-4">
                             <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                                <img class="img-fluid w-100" src="img/Just_arrived/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>" 
-                                alt="<?php echo $data['p_name']; ?>>
+                            <img 
+                             src="img/Just_arrived/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>" 
+                            alt="<?php echo $data['p_name']; ?>" 
+                            class="img-fluid w-100"
+                            style="max-height: 300px; object-fit: cover; border-radius: 5px;"
+                             >                            
                             </div>
                             <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
                                 <h6 class="text-truncate mb-3">Colorful Stylish Shirt</h6>
