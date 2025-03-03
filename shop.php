45bde43c4288757de437e355afd99be6239652f4
sorawichpanya <65010914634@msu.ac.th>
2025-03-03 14:12:11 +0700

    ฟ

--- shop.php
+++ shop.php
@@ -233,41 +233,43 @@
                         </div>
                     </div>
                     <?php
-                    include_once("connectdb.php");
-                    $sql = "SELECT * FROM `Just_arrived` ORDER BY `p_id` ASC";
-                    $rs = mysqli_query($conn , $sql);
-
-                    ?>
-                    <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
-                            <?php
-                            while ($data = mysqli_fetch_array($rs)){
-
-                            ?>
-                        <div class="card product-item border-0 mb-4">
-                            <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                            <img 
-                             src="img/Just_arrived/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>" 
-                            alt="<?php echo $data['p_name']; ?>" 
-                            class="img-fluid w-100"
-                            style="max-height: 300px; object-fit: cover; border-radius: 5px;"
-                             >                            
-                            </div>
-                            <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                            <h6 class="text-truncate mb-3"><?php echo $data['p_name']; ?></h6>
-                                <div class="d-flex justify-content-center">
-                                    <!-- แสดงราคา -->
-                                    <h6>฿<?php echo $data['p_price']; ?></h6>
-                                </div>
-                            </div>
-                            <div class="card-footer d-flex justify-content-between bg-light border">
-                                <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
-                                <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
-                            </div>
-                        </div>
-                        <?php
-                        }
-                        ?>
-                    </div>
+include_once("connectdb.php");
+$sql = "SELECT * FROM `Just_arrived` ORDER BY `p_id` ASC";
+$rs = mysqli_query($conn , $sql);
+?>
+<div class="row pb-3">
+    <?php
+    while ($data = mysqli_fetch_array($rs)) {
+    ?>
+    <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
+        <div class="card product-item border-0 mb-4 shadow-sm">
+            <div class="card-header product-img position-relative overflow-hidden bg-transparent border-0 p-0">
+                <img 
+                    src="img/Just_arrived/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>" 
+                    alt="<?php echo $data['p_name']; ?>" 
+                    class="img-fluid w-100"
+                    style="max-height: 300px; object-fit: cover; border-radius: 5px;">
+            </div>
+            <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                <h6 class="text-truncate mb-3"><?php echo $data['p_name']; ?></h6>
+                <div class="d-flex justify-content-center">
+                    <h6>฿<?php echo $data['p_price']; ?></h6>
+                </div>
+            </div>
+            <div class="card-footer d-flex justify-content-between bg-light border-0">
+                <a href="detail.php?p_id=<?php echo $data['p_id']; ?>" class="btn btn-sm text-dark p-0">
+                    <i class="fas fa-eye text-primary mr-1"></i>View Detail
+                </a>
+                <a href="#" class="btn btn-sm text-dark p-0">
+                    <i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart
+                </a>
+            </div>
+        </div>
+    </div>
+    <?php
+    }
+    ?>
+</div>
                     <div class="col-12 pb-1">
                         <nav aria-label="Page navigation">
                           <ul class="pagination justify-content-center mb-3">
