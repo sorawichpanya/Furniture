2cc71fc8f02e5ec9f0dba90eb9b8f72c77a0fb3c
sorawichpanya <65010914634@msu.ac.th>
2025-03-03 16:54:31 +0700

    ฟ

--- detail.php
+++ detail.php
@@ -330,6 +330,10 @@ $rs = mysqli_query($conn, $sql);
                             <div class="d-flex justify-content-center">
                                 <!-- แสดงราคา -->
                                 <h6>฿<?php echo number_format($data['p_price'], 2); ?></h6>
+                                <div class="card-footer d-flex justify-content-between bg-light border">
+                                <a href="detail.php?p_id=<?php echo $data['p_id']; ?>&category=Just_arrived" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
+                                <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
+
                             </div>
                         </div>
                     </div>
