77c0da58e2c885a111ab66e3c9a35f9290e2021f
sorawichpanya <65010914634@msu.ac.th>
2025-03-03 14:09:42 +0700

    a

--- shop.php
+++ shop.php
@@ -253,9 +253,10 @@
                              >                            
                             </div>
                             <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                                <h6 class="text-truncate mb-3">Colorful Stylish Shirt</h6>
+                            <h6 class="text-truncate mb-3"><?php echo $data['p_name']; ?></h6>
                                 <div class="d-flex justify-content-center">
-                                    <h6>$123.00</h6><h6 class="text-muted ml-2"><del>$123.00</del></h6>
+                                    <!-- แสดงราคา -->
+                                    <h6>฿<?php echo $data['p_price']; ?></h6>
                                 </div>
                             </div>
                             <div class="card-footer d-flex justify-content-between bg-light border">
