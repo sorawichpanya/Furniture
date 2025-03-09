9914189d4dc3398cfa54cd5f961d0dba0c43ba06
sorawichpanya <65010914634@msu.ac.th>
2025-03-09 19:19:05 +0700

    a

--- garden.php
+++ garden.php
@@ -276,7 +276,7 @@ $offset = ($page - 1) * $items_per_page; // คำนวณ offset สำหร�
 
 // ดึงข้อมูลจากทั้งสองตารางที่คละกัน
 $sql = "
-    SELECT p_id, p_name, p_price, p_ext, 'graden AS category FROM `workroom`
+    SELECT p_id, p_name, p_price, p_ext, 'graden AS category FROM `graden`
     ORDER BY p_id ASC -- กำหนดการเรียงลำดับตาม p_id
     LIMIT $items_per_page OFFSET $offset"; // ใช้ LIMIT และ OFFSET เพื่อแบ่งหน้า
 $rs = mysqli_query($conn , $sql);
--- shop.php
+++ shop.php
@@ -175,7 +175,7 @@
 
                     checkboxes.forEach(checkbox => {
                         if (checkbox.checked && checkbox.id !== "price-all") {
-                            let range = checkbox.nextElementSibling.textContent.trim().replace(/[0-9.]/g, "").replaceAll("฿", "").split(" - ")
+                            let range = checkbox.nextElementSibling.textContent.trim().replaceAll("฿", "").split(" - ")
                             console.log(range)
                             selectedRanges.push(range.map(Number)); // แปลงเป็นตัวเลข
                         }
