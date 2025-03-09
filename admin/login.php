c32f34515922da29664147ebcb5936d97fd8a782
sorawichpanya <65010914634@msu.ac.th>
2025-03-09 18:32:05 +0700

    Co-authored-by: pepypepx <potpat888@users.noreply.github.com> Co-authored-by: sstxrd <sstxrd@users.noreply.github.com> Co-authored-by: Anutraporn <Anutraporn@users.noreply.github.com> Co-authored-by: AA-261 <AA-261@users.noreply.github.com>

--- admin/login.php
+++ admin/login.php
@@ -77,54 +77,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                   <span class="sr-only">(current)</span>
                 </a>
               </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i class="far fa-file-alt"></i>
-                  <span> Reports <i class="fas fa-angle-down"></i> </span>
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="#">Daily Report</a>
-                  <a class="dropdown-item" href="#">Weekly Report</a>
-                  <a class="dropdown-item" href="#">Yearly Report</a>
-                </div>
-              </li>
               <li class="nav-item">
                 <a class="nav-link" href="products.php">
                   <i class="fas fa-shopping-cart"></i> Products
                 </a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="accounts.php">
-                  <i class="far fa-user"></i> Accounts
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i class="fas fa-cog"></i>
-                  <span> Settings <i class="fas fa-angle-down"></i> </span>
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="#">Profile</a>
-                  <a class="dropdown-item" href="#">Billing</a>
-                  <a class="dropdown-item" href="#">Customize</a>
-                </div>
               </li>
             </ul>
           </div>
