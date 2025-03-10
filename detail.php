c3c04c9a2b4587fe94104567dd9d853fe2854e79
sorawichpanya <65010914634@msu.ac.th>
2025-03-10 15:36:42 +0700

    ฟ

--- detail.php
+++ detail.php
@@ -281,13 +281,13 @@ if (isset($_GET['p_id']) && isset($_GET['category'])) {
                     <div class="d-flex align-items-center mb-4 pt-2">
     <div class="input-group quantity mr-3" style="width: 130px;">
         <div class="input-group-btn">
-            <button class="btn btn-primary btn-minus" type="button" onclick="updateQuantity(-1)">
+            <button class="btn btn-primary btn-minus" >
                 <i class="fa fa-minus"></i>
             </button>
         </div>
         <input type="text" class="form-control bg-secondary text-center" id="quantity" name="quantity" value="1" readonly>
         <div class="input-group-btn">
-            <button class="btn btn-primary btn-plus" type="button" onclick="updateQuantity(1)">
+            <button class="btn btn-primary btn-plus">
                 <i class="fa fa-plus"></i>
             </button>
         </div>
