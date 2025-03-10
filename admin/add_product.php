6e9e36650f1fed3a73f00bd0058bb98bbaf102de
sorawichpanya <65010914634@msu.ac.th>
2025-03-11 00:21:11 +0700

    a

--- admin/add_product.php
+++ admin/add_product.php
@@ -2,7 +2,8 @@
 include_once("connectdb.php");
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+var_dump($_POST);
+var_dump($_GET);
 var_dump($_FILES);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
