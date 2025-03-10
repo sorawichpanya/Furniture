1489a4aac98f5820d6796a4b977d8c2cf87c47cf
sorawichpanya <65010914634@msu.ac.th>
2025-03-10 17:32:10 +0700

    ฟ

--- Insert_register.php
+++ Insert_register.php
@@ -4,35 +4,28 @@ ini_set('display_errors', 1);
 
 include 'connectdb.php';
 
-
 // รับค่าจากฟอร์ม
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-
-$password=hash('Sha512',$password);
-
+// เข้ารหัสรหัสผ่าน
+$password = hash('sha512', $password);
 
 // เพิ่มข้อมูลลงฐานข้อมูล
-$sql = "INSERT INTO Register(name, phone, username, password)
-VALUES ('$name', '$phone', '$username', '$password')";
-
+$sql = "INSERT INTO Register(name, phone, username, password) VALUES ('$name', '$phone', '$username', '$password')";
 
 $result = mysqli_query($conn, $sql);
 
-
 if ($result) {
-    header("Location: Login.php");  
-}
-   
-    exit(); // หยุดการทำงานของ PHP
- else {
+    header("Location: Login.php");
+    exit(); // หยุดการทำงานของ PHP หลัง Redirect
+} else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     echo "<script> alert('บันทึกข้อมูลไม่ได้'); </script>";
 }
 
-
+// ปิดการเชื่อมต่อฐานข้อมูล
 mysqli_close($conn);
 ?>
