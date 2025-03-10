6b5b38267d7a380d209db1e95970a15df657f368
sorawichpanya <65010914634@msu.ac.th>
2025-03-10 16:14:55 +0700

    ฟ

--- checkout.php
+++ checkout.php
@@ -168,65 +168,36 @@ session_start(); // เรียกใช้งาน session
                 <div class="mb-4">
                     <h4 class="font-weight-semi-bold mb-4">Billing Address</h4>
                     <div class="row">
-                        <div class="col-md-6 form-group">
-                            <label>First Name</label>
-                            <input class="form-control" type="text" placeholder="John">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Last Name</label>
-                            <input class="form-control" type="text" placeholder="Doe">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>E-mail</label>
-                            <input class="form-control" type="text" placeholder="example@email.com">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Mobile No</label>
-                            <input class="form-control" type="text" placeholder="+123 456 789">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Address Line 1</label>
-                            <input class="form-control" type="text" placeholder="123 Street">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Address Line 2</label>
-                            <input class="form-control" type="text" placeholder="123 Street">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Country</label>
-                            <select class="custom-select">
-                                <option selected>United States</option>
-                                <option>Afghanistan</option>
-                                <option>Albania</option>
-                                <option>Algeria</option>
-                            </select>
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>City</label>
-                            <input class="form-control" type="text" placeholder="New York">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>State</label>
-                            <input class="form-control" type="text" placeholder="New York">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>ZIP Code</label>
-                            <input class="form-control" type="text" placeholder="123">
-                        </div>
-                        <div class="col-md-12 form-group">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="newaccount">
-                                <label class="custom-control-label" for="newaccount">Create an account</label>
-                            </div>
-                        </div>
-                        <div class="col-md-12 form-group">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="shipto">
-                                <label class="custom-control-label" for="shipto"  data-toggle="collapse" data-target="#shipping-address">Ship to different address</label>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-md-6 form-group">
+                    <label>Full Name</label>
+                    <input class="form-control" type="text" name="full_name" placeholder="John Doe" required>
                 </div>
+                <div class="col-md-6 form-group">
+                    <label>Phone Number</label>
+                    <input class="form-control" type="text" name="phone" placeholder="09XXXXXXXX" required>
+                </div>
+                <div class="col-md-12 form-group">
+                    <label>Address</label>
+                    <textarea class="form-control" name="address" rows="2" placeholder="123 Street, District" required></textarea>
+                </div>
+                <div class="col-md-6 form-group">
+                    <label>Province</label>
+                    <input class="form-control" type="text" name="province" placeholder="Bangkok" required>
+                </div>
+                <div class="col-md-6 form-group">
+                    <label>Postal Code</label>
+                    <input class="form-control" type="text" name="zip_code" placeholder="10110" required>
+                </div>
+            </div>
+
+            <div class="card-footer border-secondary bg-transparent">
+                <button type="submit" class="btn btn-lg btn-block btn-primary font-weight-bold my-3 py-3"
+                    <?php echo isset($_SESSION['payment_uploaded']) ? '' : 'disabled'; ?>>
+                    Confirm Order
+                </button>
+            </div>
+        </form>
+    </div>
             </div>
             <div class="col-lg-4">
             <div class="card border-secondary mb-5">
